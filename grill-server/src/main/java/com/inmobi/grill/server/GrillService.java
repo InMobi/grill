8fe99036b211e31730342542632d780fba10e6e5
Amareshwari Sriramadasu <amareshwari.sr@inmobi.com>
2014-03-10 11:46:31 +0530

    Add persist state and restart grill services
    Conflicts:
    
    	grill-server/src/main/java/com/inmobi/grill/server/GrillService.java
    	grill-server/src/main/java/com/inmobi/grill/server/GrillServices.java
    	grill-server/src/main/resources/grill-default.xml
    	grill-server/src/test/resources/grill-site.xml

--- grill-server/src/main/java/com/inmobi/grill/server/GrillService.java
+++ grill-server/src/main/java/com/inmobi/grill/server/GrillService.java
@@ -20,6 +20,10 @@ package com.inmobi.grill.server;
  * #L%
  */
 
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -41,7 +45,7 @@ import com.inmobi.grill.api.GrillException;
 import com.inmobi.grill.api.GrillSessionHandle;
 import com.inmobi.grill.server.session.GrillSessionImpl;
 
-public abstract class GrillService extends CompositeService {
+public abstract class GrillService extends CompositeService implements Externalizable {
 
   private final CLIService cliService;
 
@@ -168,6 +172,15 @@ public abstract class GrillService extends CompositeService {
       }
     }
     return qconf;
+  }
+
+  @Override
+  public void readExternal(ObjectInput in) throws IOException,
+      ClassNotFoundException {
+  }
+
+  @Override
+  public void writeExternal(ObjectOutput out) throws IOException {
   }
 
 }
