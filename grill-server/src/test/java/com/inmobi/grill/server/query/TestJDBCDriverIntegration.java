74164b3e62b188234e12f856b014c53512e4b431
jdhok <jaideep.dhok@inmobi.com>
2014-07-23 16:30:29 +0530

    Added test for persistent result set for JDBC result

--- grill-server/src/test/java/com/inmobi/grill/server/query/TestJDBCDriverIntegration.java
+++ grill-server/src/test/java/com/inmobi/grill/server/query/TestJDBCDriverIntegration.java
@@ -1,19 +1,26 @@
 package com.inmobi.grill.server.query;
 
+import com.inmobi.grill.api.GrillConf;
 import com.inmobi.grill.api.GrillException;
 import com.inmobi.grill.api.GrillSessionHandle;
 import com.inmobi.grill.api.query.*;
 import com.inmobi.grill.driver.hive.HiveDriver;
 import com.inmobi.grill.driver.jdbc.JDBCDriver;
 import com.inmobi.grill.server.GrillServices;
+import com.inmobi.grill.server.api.GrillConfConstants;
 import com.inmobi.grill.server.api.metrics.MetricsService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hive.conf.HiveConf;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -21,9 +28,7 @@ import java.sql.Statement;
 import java.util.HashMap;
 import java.util.List;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 
 @Test(groups = "integration")
@@ -172,6 +177,11 @@ public class TestJDBCDriverIntegration extends TestQueryService {
   void validatePersistentResult(QueryResult resultset, QueryHandle handle, boolean isDir) throws IOException {
     if (resultset instanceof InMemoryQueryResult) {
       validateInmemoryResult((InMemoryQueryResult) resultset);
+    } else if (resultset instanceof PersistentQueryResult) {
+      LOG.info(">> Validating persistent query result "
+        + ((PersistentQueryResult) resultset).getPersistedURI());
+      List<String> rows = readResultSet((PersistentQueryResult) resultset, handle, isDir);
+      assertEquals(rows.size(), NUM_RECORDS);
     }
   }
 
@@ -188,6 +198,52 @@ public class TestJDBCDriverIntegration extends TestQueryService {
     assertTrue(metadata.getColumns().get(1).getName().toLowerCase().equals((outputTablePfx + "IDSTR").toLowerCase()) ||
       metadata.getColumns().get(0).getName().toLowerCase().equals("IDSTR".toLowerCase()));
     assertEquals(metadata.getColumns().get(1).getType().name().toLowerCase(), "VARCHAR".toLowerCase());
+  }
+
+  @Test
+  public void testJDBCPersistentResultSet() throws Exception {
+    final WebTarget target = target().path("queryapi/queries");
+    final GrillConf conf = new GrillConf();
+    final FormDataMultiPart mp = new FormDataMultiPart();
+    conf.addProperty(GrillConfConstants.GRILL_PERSISTENT_RESULT_SET, "true");
+    conf.addProperty(GrillConfConstants.QUERY_PERSISTENT_RESULT_INDRIVER, "false");
+    //conf.addProperty(GrillConfConstants.QUERY_OUTPUT_SERDE, LazySimpleSerDe.class.getCanonicalName());
+    mp.bodyPart(new FormDataBodyPart(FormDataContentDisposition.name("sessionid").build(),
+      grillSessionId, MediaType.APPLICATION_XML_TYPE));
+    mp.bodyPart(new FormDataBodyPart(FormDataContentDisposition.name("query").build(),
+      "select ID from " + testTable));
+    mp.bodyPart(new FormDataBodyPart(FormDataContentDisposition.name(
+      "operation").build(),
+      "execute"));
+    mp.bodyPart(new FormDataBodyPart(
+      FormDataContentDisposition.name("conf").fileName("conf").build(),
+      conf,
+      MediaType.APPLICATION_XML_TYPE));
+    QueryHandle handle = target.request().post(
+      Entity.entity(mp, MediaType.MULTIPART_FORM_DATA_TYPE), QueryHandle.class);
+
+    Assert.assertNotNull(handle);
+
+    // Get query
+    GrillQuery ctx = target.path(handle.toString()).queryParam("sessionid",
+      grillSessionId).request().get(GrillQuery.class);
+    // wait till the query finishes
+    QueryStatus stat = ctx.getStatus();
+    while (!stat.isFinished()) {
+      ctx = target.path(handle.toString()).queryParam("sessionid",
+        grillSessionId).request().get(GrillQuery.class);
+      stat = ctx.getStatus();
+      Thread.sleep(1000);
+    }
+
+    if (stat.getStatus().equals(QueryStatus.Status.SUCCESSFUL)) {
+      // fetch results
+      QueryResult resultset = target.path(handle.toString()).path(
+        "resultset").queryParam("sessionid", grillSessionId).request().get(QueryResult.class);
+      validatePersistentResult(resultset, handle, false);
+    } else {
+      fail("Query failed ");
+    }
   }
 
   @Override
