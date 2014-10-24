8dd83f31ca16aa626312e05b002b366f93b7b818
jdhok <jaideep.dhok@inmobi.com>
2014-10-24 15:20:35 +0530

    Remove unused imports

--- lens-driver-jdbc/src/main/java/org/apache/lens/driver/jdbc/ColumnarSQLRewriter.java
+++ lens-driver-jdbc/src/main/java/org/apache/lens/driver/jdbc/ColumnarSQLRewriter.java
@@ -21,9 +21,6 @@ package org.apache.lens.driver.jdbc;
  */
 
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.antlr.runtime.CommonToken;
 import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
@@ -42,8 +39,6 @@ import org.apache.lens.cube.metadata.CubeMetastoreClient;
 import org.apache.lens.cube.parse.CubeSemanticAnalyzer;
 import org.apache.lens.cube.parse.HQLParser;
 import org.apache.lens.server.api.LensConfConstants;
-import org.apache.log4j.Logger;
-
 import static org.apache.hadoop.hive.ql.parse.HiveParser.*;
 
 public class ColumnarSQLRewriter implements QueryRewriter {
