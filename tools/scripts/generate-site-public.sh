8654f3fe6802b0b8160eb14238d123702423f0b2
Amareshwari Sriramadasu <amareshwari@apache.org>
2015-02-26 09:57:53 +0530

    Revert changes from apache on tools/scripts/generate-site-public.sh

--- tools/scripts/generate-site-public.sh
+++ tools/scripts/generate-site-public.sh
@@ -1,76 +1,53 @@
 #!/bin/sh
-#
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-#
-
 
 die() {
     echo "$1"
     exit 1
 }
 
-usage="usage: generated-site-public.sh <target site location on svn>"
-
-# if no args specified, show usage
-if [ $# != 1 ]; then
-  echo $usage
-  exit 1
-fi
-
-SVN_TARGET=$1
-TMP=/tmp/lens-site-stage
+REPO=git@github.com:InMobi/grill.git
+TMP=/tmp/grill-site-stage
 STAGE=`pwd`/target/staging
-REST_DIR=`pwd`/lens-server/target/site/wsdocs
+REST_DIR=`pwd`/grill-server/target/site/wsdocs
+IMAGES_DIR=`pwd`/src/site/apt/figures
+LOGO_FILE=`pwd`/grill-logo.png
 VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version|grep -Ev '(^\[|Download\w+:)' || die "unable to get version")
 
 
 echo "Starting generate-site"
 CURR_BRANCH=`git branch | sed -n '/\* /s///p'`
-echo "Running site in current lens branch" $CURR_BRANCH
+echo "Running site in current grill branch" $CURR_BRANCH
 mvn clean test -Dtest=TestGenerateConfigDoc || die "Unable to generate config docs"
 mvn install -DskipTests
-mvn site site:stage -Ddependency.locations.enabled=false -Ddependency.details.enabled=false || die "unable to generate site"
-cd lens-server
+mvn clean site site:stage -Ddependency.locations.enabled=false -Ddependency.details.enabled=false || die "unable to generate site"
+cd grill-server
 mvn enunciate:docs
 cd ..
 echo "Site gen complete"
 
 rm -rf $TMP || die "unable to clear $TMP"
-mkdir -p $TMP
 
+echo "Beginning push to gh-pages from " $CURR_BRANCH
+git clone $REPO $TMP || die "unable to clone $TMP"
 cd $TMP
+git checkout gh-pages || die "unable to checkout gh-pages"
 
 mkdir -p current || die "unable to create dir current"
+mkdir -p wsdocs || die "Unable to create dir for REST docs"
 mkdir -p versions/$VERSION || due "unable to create dir versions/$VERSION"
 
 find current -type f -exec git rm {} \;
 echo "Copying REST docs from " $REST_DIR
+cp $LOGO_FILE .
 # Delete index.html from the source wsdocs as it conflitcs with maven index.html
 echo "DELETE $REST_DIR/index.html"
 rm $REST_DIR/index.html
-echo "removing generated jars from the REST directory"
-rm $REST_DIR/*.jar
 echo "Copy enunciate documentation"
 cp -r $REST_DIR/* .
-cp -r $REST_DIR/* current/ || die "unable to copy REST to current"
-cp -r $REST_DIR/* versions/$VERSION/ || die "unable to copy REST to versions/$VERSION"
+echo "Copy images"
+cp -r $IMAGES_DIR . 
 echo "Copy MVN site"
-cp -r $STAGE/ .
+cp -r $STAGE/ . || die "unable to copy to base"
 echo "Copy docs to current/"
 cp -r $STAGE/ current/ || die "unable to copy to current"
 echo "Copy docs to version:" $VERSION
@@ -84,9 +61,9 @@ do
 done
 echo '</ul>' >> versions/index.html
 
+git add . || die "unable to add for commit"
+git commit -m "Updated documentation for version $VERSION. Source branch $CURR_BRANCH" || die "unable to commit to git"
+git push origin gh-pages || die "unable to push to gh-pages"
 
-## Copy entire doc directory to Apache SVN Target dir
-mkdir -p $SVN_TARGET/site/publish
-cp -r $TMP/ $SVN_TARGET/site/publish
-cd $SVN_TARGET
-echo "Generated site."
+cd $STAGE
+rm -rf $TMP || die "unable to clear $TMP"
